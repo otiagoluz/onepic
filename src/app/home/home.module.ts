829e9ebca12cbd2a3a6026e0e32b95ad7c772bdb
otiagoluz <tiagoluzgpr@gmail.com>
2020-05-11 11:00:20 -0400

    add signup

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,19 +1,24 @@
 
 import { NgModule } from "@angular/core";
-import { ReactiveFormsModule } from '@angular/forms'
+import { ReactiveFormsModule, FormsModule } from '@angular/forms'
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
 import { SignInComponent } from './signin/signin.component';
 import { VMessageModule } from '../shared/components/vmessage/vmessage.module';
+import { SignUpComponent } from './signup/signup.component';
 
 @NgModule({
-  declarations: [SignInComponent],
+  declarations: [
+    SignInComponent,
+    SignUpComponent
+  ],
   imports: [ 
     CommonModule,
     ReactiveFormsModule,
     VMessageModule,
-    RouterModule
+    RouterModule,
+    FormsModule
   ]
 })
 
